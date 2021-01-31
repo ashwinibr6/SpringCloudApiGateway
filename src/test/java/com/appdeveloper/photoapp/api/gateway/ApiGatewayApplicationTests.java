854cf88474c03fe35402cd4085f4e83475d133cd
ashwinibr6 <ashwinibr6@gmail.com>
2021-01-30 19:51:36 -0600

    first commit

---
+++ src/test/java/com/appdeveloper/photoapp/api/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdeveloper.photoapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
