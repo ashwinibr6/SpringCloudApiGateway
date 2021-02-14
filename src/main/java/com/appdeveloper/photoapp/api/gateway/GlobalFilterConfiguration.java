f58cb3f80934b463205b554d2843554f218094e0
ashwinibr6 <ashwinibr6@gmail.com>
2021-02-14 16:26:22 -0600

    Api gateway pre and post filters

---
+++ src/main/java/com/appdeveloper/photoapp/api/gateway/GlobalFilterConfiguration.java
@@ -0,0 +1,46 @@
+package com.appdeveloper.photoapp.api.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GlobalFilterConfiguration {
+
+	final Logger logger = LoggerFactory.getLogger(GlobalFilterConfiguration.class);
+	
+	@Order(1)
+	@Bean
+	public GlobalFilter secondPreFilter() {
+		
+		
+		return (exchange, chain)->{
+			logger.info("My second global pre filter is executed");
+			return chain.filter(exchange).then(Mono.fromRunnable(()->{
+				logger.info("My second global post-filter was executed..");
+			}));
+		};
+		
+	}
+	
+	@Order(2)
+	@Bean
+	public GlobalFilter thirdPreFilter() {
+		
+		
+		return (exchange, chain)->{
+			logger.info("My third global pre filter is executed");
+			return chain.filter(exchange).then(Mono.fromRunnable(()->{
+				logger.info("My third global post-filter was executed..");
+			}));
+		};
+		
+	}
+	
+	
+}
